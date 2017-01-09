52cc60965c3b5e7bc1181f976c41fed5ec1c92f6
Zoran Todorovic <zoran@arsfutura.co>
2017-01-09 18:18:31 +0100

    Changed XOR into both-ways neural network.

--- src/XOR.java
+++ src/XOR.java
@@ -7,66 +7,117 @@
 
 import org.joone.engine.*;
 import org.joone.engine.learning.TeachingSynapse;
+import org.joone.io.FileInputSynapse;
 import org.joone.io.FileOutputSynapse;
-import org.joone.io.MemoryInputSynapse;
 import org.joone.io.MemoryOutputSynapse;
 import org.joone.net.NeuralNet;
 
 
 public class XOR implements NeuralNetListener {
-    private NeuralNet nnet = null;
-    private MemoryInputSynapse inputSynapse;
-    private MemoryInputSynapse desiredOutputSynapse;
-    private MemoryOutputSynapse outputSynapse;
-    private double[][] inputArray = new double[][]{{0.0D, 0.0D}, {0.0D, 1.0D}, {1.0D, 0.0D}, {1.0D, 1.0D}};
-    private double[][] desiredOutputArray = new double[][]{{0.0D}, {1.0D}, {1.0D}, {0.0D}};
+    private NeuralNet nnet1 = null;
+    private NeuralNet nnet2 = null;
+    private FileInputSynapse firstInputSynapse;
+    private FileInputSynapse secondInputSynapse;
+    private FileInputSynapse desiredFirstOutputSynapse;
+    private FileInputSynapse desiredSecondOutputSynapse;
+    private MemoryOutputSynapse firstOutputSynapse;
+    private MemoryOutputSynapse secondOutputSynapse;
 
     public XOR() {
     }
 
     public static void main(String[] args) {
         XOR xor = new XOR();
-        xor.initNeuralNet();
-        xor.train();
+        xor.initFirstNN();
+        System.out.println("First NN initialized");
+        xor.trainFirstNN();
+        System.out.println("First NN trained");
+        xor.interrogateFirstNN();
+        System.out.println("First NN interrogated");
+
+        xor.initSecondNN();
+        System.out.println("Second NN initialized");
+        xor.trainSecondNN();
+        System.out.println("Second NN trained");
+        xor.interrogateSecondNN();
+        System.out.println("Second NN interrogated");
     }
 
-    public void train() {
-        this.inputSynapse.setInputArray(this.inputArray);
-        this.inputSynapse.setAdvancedColumnSelector("1,2");
-        this.desiredOutputSynapse.setInputArray(this.desiredOutputArray);
-        this.desiredOutputSynapse.setAdvancedColumnSelector("1");
-        Monitor monitor = this.nnet.getMonitor();
+
+    public void trainFirstNN() {
+        this.firstInputSynapse.setFileName("in_first.txt");
+        this.firstInputSynapse.setAdvancedColumnSelector("1,2");
+        this.desiredFirstOutputSynapse.setFileName("in_first.txt");
+        this.desiredFirstOutputSynapse.setAdvancedColumnSelector("3");
+        Monitor monitor = this.nnet1.getMonitor();
         monitor.setLearningRate(0.8D);
         monitor.setMomentum(0.3D);
-        monitor.setTrainingPatterns(this.inputArray.length);
-        monitor.setTotCicles(100000);
+        monitor.setTrainingPatterns(4);
+        monitor.setTotCicles(100);
         monitor.setLearning(true);
-        this.nnet.addNeuralNetListener(this);
-        this.nnet.start();
-        this.nnet.getMonitor().Go();
-        this.nnet.join();
-        System.out.println("Network stopped. Last RMSE=" + this.nnet.getMonitor().getGlobalError());
-        this.interrogate();
+        this.nnet1.addNeuralNetListener(this);
+        this.nnet1.start();
+        this.nnet1.getMonitor().Go();
+        this.nnet1.join();
+        System.out.println("Network stopped. Last RMSE=" + this.nnet1.getMonitor().getGlobalError());
+//        this.interrogateFirstNN();
     }
 
-    private void interrogate() {
-        Monitor monitor = this.nnet.getMonitor();
+    public void trainSecondNN() {
+//        this.inputSynapse.setInputArray(this.inputArray);
+        this.secondInputSynapse.setFileName("in_first.txt");
+        this.secondInputSynapse.setAdvancedColumnSelector("3");
+//        this.desiredOutputSynapse.setInputArray(this.desiredOutputArray);
+        this.desiredSecondOutputSynapse.setFileName("in_first.txt");
+        this.desiredSecondOutputSynapse.setAdvancedColumnSelector("1,2");
+        Monitor monitor = this.nnet2.getMonitor();
+        monitor.setLearningRate(0.8D);
+        monitor.setMomentum(0.3D);
+        monitor.setTrainingPatterns(4);
+        monitor.setTotCicles(100);
+        monitor.setLearning(true);
+        this.nnet2.addNeuralNetListener(this);
+        this.nnet2.start();
+        this.nnet2.getMonitor().Go();
+        this.nnet2.join();
+        System.out.println("Network stopped. Last RMSE=" + this.nnet2.getMonitor().getGlobalError());
+        System.out.println("Second network interrogation started");
+        this.interrogateSecondNN();
+    }
+
+    private void interrogateFirstNN() {
+        Monitor monitor = this.nnet1.getMonitor();
         monitor.setTrainingPatterns(4);
         monitor.setTotCicles(1);
         monitor.setLearning(false);
         FileOutputSynapse output = new FileOutputSynapse();
-        output.setFileName("xor_out.txt");
-        if(this.nnet != null) {
-            this.nnet.addOutputSynapse(output);
-            System.out.println(this.nnet.check());
-            this.nnet.start();
+        output.setFileName("out_first.txt");
+        if(this.nnet1 != null) {
+            this.nnet1.addOutputSynapse(output);
+            System.out.println(this.nnet1.check());
+            this.nnet1.start();
             monitor.Go();
-            this.nnet.join();
+            this.nnet1.join();
         }
-
     }
 
-    protected void initNeuralNet() {
+    private void interrogateSecondNN() {
+        Monitor monitor = this.nnet2.getMonitor();
+        monitor.setTrainingPatterns(4);
+        monitor.setTotCicles(1);
+        monitor.setLearning(false);
+        FileOutputSynapse output = new FileOutputSynapse();
+        output.setFileName("out_second.txt");
+        if(this.nnet2 != null) {
+            this.nnet2.addOutputSynapse(output);
+            System.out.println(this.nnet2.check());
+            this.nnet2.start();
+            monitor.Go();
+            this.nnet2.join();
+        }
+    }
+
+    protected void initFirstNN() {
         LinearLayer input = new LinearLayer();
         SigmoidLayer hidden = new SigmoidLayer();
         SigmoidLayer output = new SigmoidLayer();
@@ -76,24 +127,61 @@ public class XOR implements NeuralNetListener {
         input.setLayerName("L.input");
         hidden.setLayerName("L.hidden");
         output.setLayerName("L.output");
+
         FullSynapse synapse_IH = new FullSynapse();
         FullSynapse synapse_HO = new FullSynapse();
         input.addOutputSynapse(synapse_IH);
         hidden.addInputSynapse(synapse_IH);
         hidden.addOutputSynapse(synapse_HO);
         output.addInputSynapse(synapse_HO);
-        this.inputSynapse = new MemoryInputSynapse();
-        input.addInputSynapse(this.inputSynapse);
-        this.outputSynapse = new MemoryOutputSynapse();
-        output.addOutputSynapse(this.outputSynapse);
-        this.desiredOutputSynapse = new MemoryInputSynapse();
+
+        this.firstInputSynapse = new FileInputSynapse();
+        input.addInputSynapse(this.firstInputSynapse);
+        this.firstOutputSynapse = new MemoryOutputSynapse();
+        output.addOutputSynapse(this.firstOutputSynapse);
+//        this.desiredOutputSynapse = new MemoryInputSynapse();
         TeachingSynapse trainer = new TeachingSynapse();
-        trainer.setDesired(this.desiredOutputSynapse);
-        this.nnet = new NeuralNet();
-        this.nnet.addLayer(input, 0);
-        this.nnet.addLayer(hidden, 1);
-        this.nnet.addLayer(output, 2);
-        this.nnet.setTeacher(trainer);
+        this.desiredFirstOutputSynapse = new FileInputSynapse();
+        trainer.setDesired(this.desiredFirstOutputSynapse);
+        this.nnet1 = new NeuralNet();
+        this.nnet1.addLayer(input, 0);
+        this.nnet1.addLayer(hidden, 1);
+        this.nnet1.addLayer(output, 2);
+        this.nnet1.setTeacher(trainer);
+        output.addOutputSynapse(trainer);
+    }
+
+    protected void initSecondNN(){
+        LinearLayer input = new LinearLayer();
+        SigmoidLayer hidden = new SigmoidLayer();
+        SigmoidLayer output = new SigmoidLayer();
+        input.setRows(1);
+        hidden.setRows(3);
+        output.setRows(2);
+        input.setLayerName("L2.input");
+        hidden.setLayerName("L2.hidden");
+        output.setLayerName("L2.output");
+
+        FullSynapse synapse_IH = new FullSynapse();
+        FullSynapse synapse_HO = new FullSynapse();
+        input.addOutputSynapse(synapse_IH);
+        hidden.addInputSynapse(synapse_IH);
+        hidden.addOutputSynapse(synapse_HO);
+        output.addInputSynapse(synapse_HO);
+
+        this.secondInputSynapse = new FileInputSynapse();
+        input.addInputSynapse(this.secondInputSynapse);
+        this.secondOutputSynapse = new MemoryOutputSynapse();
+        output.addOutputSynapse(this.secondOutputSynapse);
+//        this.desiredOutputSynapse = new MemoryInputSynapse();
+        TeachingSynapse trainer = new TeachingSynapse();
+        this.desiredSecondOutputSynapse = new FileInputSynapse();
+        trainer.setDesired(this.desiredSecondOutputSynapse);
+        this.nnet2 = new NeuralNet();
+        this.nnet2.addLayer(input, 0);
+        this.nnet2.addLayer(hidden, 1);
+        this.nnet2.addLayer(output, 2);
+        this.nnet2.setTeacher(trainer);
         output.addOutputSynapse(trainer);
     }
 
